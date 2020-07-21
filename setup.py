5e255355c1a99cd6840db64dba537994dcc570a2
panxu <panxu@baidu.com>
2020-07-21 10:53:32 +0800

    Releaset: v0.0.3

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import find_packages
 
 setup(
     name="easytext-nlp",
-    version="0.0.2",
+    version="0.0.3",
     author="Pan Xu",
     author_email="cjopengler@163.com",
     description="Make it easy to train and metric NLP model.",
