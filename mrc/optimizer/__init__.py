d55b8f2d34bf22354f6f208f2ce893a71c6b6dfa
panxu <cjopengler@163.com>
2021-11-05 09:38:08 +0800

    #52: 完成 optimizer 和 lr scheduler

---
+++ mrc/optimizer/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python 3
+# -*- coding: utf-8 -*-
+
+#
+# Copyright (c) 2021 PanXu, Inc. All Rights Reserved
+#
+"""
+初始化
+
+Authors: PanXu
+Date:    2021/11/05 08:49:00
+"""
+
+from .mrc_optimizer import MRCOptimizer
